e85b1451299ca4449b7a046a69ccb1c7d93238df
Bastien <bastien.guerry@data.gouv.fr>
2020-01-14 15:51:00 +0100

    Refine configuration tests

--- test/choices/test.clj
+++ test/choices/test.clj
@@ -6,9 +6,7 @@
 
 (def config (inline-yaml-resource "config.yml"))
 
-(def init-scores
-  (apply merge (map (fn [kv] {(key kv) (into {} (val kv))})
-                    (into {} (:init-scores config)))))
+(def init-scores (:init-scores config))
 
 (s/def ::name string?)
 (s/def ::text string?)
@@ -27,11 +25,16 @@
 (s/def ::display string?)
 (s/def ::value int?)
 (s/def ::result string?)
+
 (s/def ::init-score (s/keys :req-un [::display ::value ::result]))
 (s/def ::init-scores (s/map-of keyword? ::init-score))
 
+(s/def ::choice-score (s/keys :opt-un [::display ::value ::result]))
+(s/def ::score (s/map-of (into #{} (keys init-scores)) ::choice-score))
+
 (s/def ::choice (s/keys :req-un [::answer ::goto]
                         :opt-un [::color ::summary ::score ::explain]))
+
 (s/def ::choices (s/coll-of ::choice))
 
 (s/def ::branch (s/keys :req-un [::name ::text]
@@ -39,24 +42,28 @@
                                  ::help ::no-summary ::done]))
 (s/def ::tree (s/coll-of ::branch))
 
-(s/def ::email (s/or :empty nil? :with-arobase #(re-find #"@" %)))
-
+(def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
+(s/def ::email (s/or :empty nil? :with-arobase #(re-matches email-regex %)))
 (defn nilable-email? [s] (s/valid? ::email s))
+
 (defn nilable-map? [m] (s/valid? (s/nilable map?) m))
 
-(deftest ui-tests
-  (testing "Testing basic UI variables"
+(deftest ui
+  (testing "Testing UI variables"
     (is (or (= (:locale config) "en")
             (= (:locale config) "fr")))
     (is (nilable-map? (:ui-strings config)))
     (is (nilable-email? (:mail-to config)))
-    (is (s/valid? ::init-scores init-scores))
     (is (boolean? (:display-help config)))
     (is (boolean? (:display-score config)))
-    (is (map? (:init-scores config)))
     (is (map? (:header config)))
     (is (map? (:footer config)))))
 
-(deftest tree-tests
-  (testing "Testing tree format"
+(deftest score
+  (testing "Testing score setting"
+    (is (s/valid? ::init-scores init-scores))
+    (is (map? (:init-scores config)))))
+
+(deftest tree
+  (testing "Testing the options tree format"
     (is (s/valid? ::tree (:tree config)))))
