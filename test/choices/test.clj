7927f6c49195b837e69f33891ce16291ace57c4b
Bastien <bastien.guerry@data.gouv.fr>
2020-01-14 14:30:43 +0100

    Enhance configuration test

--- test/choices/test.clj
+++ test/choices/test.clj
@@ -6,6 +6,10 @@
 
 (def config (inline-yaml-resource "config.yml"))
 
+(def init-scores
+  (apply merge (map (fn [kv] {(key kv) (into {} (val kv))})
+                    (into {} (:init-scores config)))))
+
 (s/def ::name string?)
 (s/def ::text string?)
 (s/def ::answer string?)
@@ -15,29 +19,38 @@
 (s/def ::summary (s/or :simple-summary string?
                        :composed-summary (s/coll-of string?)))
 (s/def ::help string?)
-(s/def ::score map?)
 (s/def ::home-page boolean?)
 (s/def ::start-page boolean?)
 (s/def ::no-summary boolean?)
 (s/def ::done boolean?)
 
-(s/def ::choices (s/coll-of ::choice))
+(s/def ::display string?)
+(s/def ::value int?)
+(s/def ::result string?)
+(s/def ::init-score (s/keys :req-un [::display ::value ::result]))
+(s/def ::init-scores (s/map-of keyword? ::init-score))
+
 (s/def ::choice (s/keys :req-un [::answer ::goto]
                         :opt-un [::color ::summary ::score ::explain]))
+(s/def ::choices (s/coll-of ::choice))
 
-(s/def ::tree (s/coll-of ::branch))
 (s/def ::branch (s/keys :req-un [::name ::text]
                         :opt-un [::choices ::home-page ::start-page
                                  ::help ::no-summary ::done]))
+(s/def ::tree (s/coll-of ::branch))
+
+(s/def ::email (s/or :empty nil? :with-arobase #(re-find #"@" %)))
+
+(defn nilable-email? [s] (s/valid? ::email s))
+(defn nilable-map? [m] (s/valid? (s/nilable map?) m))
 
 (deftest ui-tests
   (testing "Testing basic UI variables"
     (is (or (= (:locale config) "en")
             (= (:locale config) "fr")))
-    (is (or (empty? (:ui-strings config))
-            (map? (:ui-strings config))))
-    (is (or (empty? (:mail-to config))
-            (re-find #"@" (:mail-to config))))
+    (is (nilable-map? (:ui-strings config)))
+    (is (nilable-email? (:mail-to config)))
+    (is (s/valid? ::init-scores init-scores))
     (is (boolean? (:display-help config)))
     (is (boolean? (:display-score config)))
     (is (map? (:init-scores config)))
