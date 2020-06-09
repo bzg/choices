f21a95917f79644b349a57eaf607b676e3ba9d21
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-06-09 21:25:58 +0200

    Bump dependencies versions (except Clojurescript)

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;; SPDX-License-Identifier: EPL-2.0
 ;; License-Filename: LICENSES/EPL-2.0.txt
 
-(defproject choices "0.9.3"
+(defproject choices "0.9.4"
 
   :description "Build SPAs to let users traverse choices"
   :url "https://github.com/etalab/choices"
@@ -28,10 +28,10 @@
   :profiles {:dev {:source-paths ["src/cljs"]
                    :dependencies [[org.clojure/clojurescript "1.10.597"]
                                   [markdown-to-hiccup "0.6.2"]
-                                  [com.bhauman/figwheel-main "0.2.3"]
+                                  [com.bhauman/figwheel-main "0.2.8"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
                                   [reagent "0.10.0"]
                                   [reagent-utils "0.3.3"]
                                   [cljsjs/clipboard "2.0.4-0"]
                                   [com.taoensso/tempura "1.2.1"]
-                                  [metosin/reitit-frontend "0.4.2"]]}})
+                                  [metosin/reitit-frontend "0.5.2"]]}})
