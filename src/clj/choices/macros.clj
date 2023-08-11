2fb89b426c406c7792e5c75c494e511eb36addc0
Bastien <bzg@gnu.org>
2023-08-11 10:14:07 +0200

    README: Document themes
    Also add dsfr fonts, indicating the copyright exception.

--- src/clj/choices/macros.clj
+++ src/clj/choices/macros.clj
@@ -4,7 +4,9 @@
 
 (ns choices.macros
   (:require [yaml.core :as yaml]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
 
 (defmacro inline-yaml-resource [resource-path]
   (yaml/parse-string
@@ -13,3 +15,35 @@
 (defmacro inline-edn-resource [resource-path]
   (edn/read-string
    (slurp resource-path)))
+
+(defn use-dsfr [& [args]]
+  (let [config    (slurp "config.yml")
+        newconfig (string/replace config #"theme: \"[^\"]+\"" "theme: \"dsfr\"")]
+    (spit "config.yml" newconfig)
+    (io/copy
+     (io/file "resources/public/index_dsfr.html")
+     (io/file "resources/public/index.html"))))
+
+(defn use-bulma [& [args]]
+  (let [config    (slurp "config.yml")
+        newconfig (string/replace config #"theme: \"[^\"]+\"" "theme: \"bulma\"")]
+    (spit "config.yml" newconfig)
+    (io/copy
+     (io/file "resources/public/index_bulma.html")
+     (io/file "resources/public/index.html"))))
+
+(defn use-chota [& [args]]
+  (let [config    (slurp "config.yml")
+        newconfig (string/replace config #"theme: \"[^\"]+\"" "theme: \"chota\"")]
+    (spit "config.yml" newconfig)
+    (io/copy
+     (io/file "resources/public/index_chota.html")
+     (io/file "resources/public/index.html"))))
+
+(defn set-theme [{:keys [theme]}]
+  (let [theme (name theme)]
+    (if-not theme
+      (println "Please provide a theme")
+      (cond (= theme "chota") (use-chota)
+            (= theme "dsfr")  (use-dsfr)
+            :else             (use-bulma)))))
