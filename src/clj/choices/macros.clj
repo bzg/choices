e3ab5c3b3a0c3d92800e08223e897fcc9d565680
Bastien <bzg@gnu.org>
2023-08-11 13:37:54 +0200

    Don't use dsfr
    It is not ready yet.
    Also make it easier to set themes.

--- src/clj/choices/macros.clj
+++ src/clj/choices/macros.clj
@@ -4,7 +4,9 @@
 
 (ns choices.macros
   (:require [yaml.core :as yaml]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
 
 (defmacro inline-yaml-resource [resource-path]
   (yaml/parse-string
@@ -13,3 +15,34 @@
 (defmacro inline-edn-resource [resource-path]
   (edn/read-string
    (slurp resource-path)))
+
+(defn slurp-no-error [f]
+  (try (slurp f) (catch Exception e nil)))
+
+(defn use-bulma [& [args]]
+  (if-let [config (or (not-empty (slurp-no-error "config.yml"))
+                      (slurp "config-example.yml"))]
+    (let [index (slurp "resources/public/index.html")]
+      (spit "config.yml"
+            (string/replace config #"theme: \"[^\"]+\"" "theme: \"bulma\""))
+      (spit "resources/public/index.html"
+            (string/replace index #"css/[^\"]+" "css/bulma.css")))
+    (println "Can't set bulma theme")))
+
+(defn use-chota [& [args]]
+  (if-let [config (or (not-empty (slurp-no-error "config.yml"))
+                      (slurp "config-example.yml"))]
+    (let [index (slurp "resources/public/index.html")]
+      (spit "config.yml"
+            (string/replace config #"theme: \"[^\"]+\"" "theme: \"chota\""))
+      (spit "resources/public/index.html"
+            (string/replace index #"css/[^\"]+" "css/chota.css")))
+    (println "Can't set chota theme")))
+
+(defn set-theme [{:keys [theme]}]
+  (let [theme (name theme)]
+    (if-not theme
+      (println "Please provide a theme")
+      (if (= theme "chota")
+        (use-chota)
+        (use-bulma)))))
