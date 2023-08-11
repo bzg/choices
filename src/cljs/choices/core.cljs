b8dea9b2654dd5692d93ce799819db541f83c73d
Bastien <bzg@gnu.org>
2023-08-11 18:19:54 +0200

    src/cljs/choices/core.cljs: Enhance `md-to-string`

--- src/cljs/choices/core.cljs
+++ src/cljs/choices/core.cljs
@@ -41,7 +41,7 @@
   (first (remove nil? (map #(when (:start-page %) (keyword (:node %))) tree))))
 
 (defn md-to-string [^String s]
-  (-> s (md/md->hiccup) (md/component)))
+  (-> s (md/md->hiccup) (md/component) peek))
 
 ;; History-handling variables
 (def history (reagent/atom [{:score score-variables}]))
